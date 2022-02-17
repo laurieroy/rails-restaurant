d3d2ecff100ff1b13beba3d1a1bf3432de205f19
lauretteroy <laurieroy.dev@gmail.com>
2022-02-16 23:48:47 -0800

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create!(email: "admin@example.com", password: "Password", password_confirmation: "Password", admin: true)
+puts "admin user created"
+
 25.times do |i|
 	Product.create!(
 		name: "Pizza" ,
 		description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, cumque quidem molestias fuga id facere a. Similique, doloremque est. Possimus aut facilis molestiae cumque iusto vel doloremque, animi praesentium ea?",
 		price: 11.99,
 		catering_item: true
-
 	)
+	puts "25 menu items created"
 end
