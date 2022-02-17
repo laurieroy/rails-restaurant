d768c88bef9c61ac6acf6ceab0b2b9a6d9dacc28
lauretteroy <laurieroy.dev@gmail.com>
2022-02-16 23:47:30 -0800

    Add page links

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  root 'pages#home'
-  get 'pages/catering'
-  get 'pages/info'
+  devise_for :users
   resources :products
+  get 'catering', to: 'pages#catering'
+  get 'info', to: 'pages#info'
+
+  root 'pages#home'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
