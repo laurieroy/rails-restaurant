7ff8a1298c620f6f8a53aaef99ded62a192536b9
lauretteroy <laurieroy.dev@gmail.com>
2022-02-28 19:51:58 -0800

    Add order_items, cart

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,29 @@
 class OrderItem < ApplicationRecord
   belongs_to :product
   belongs_to :order
+  before_save :set_unit_price
+  before_save :set_total
+
+  def total
+    return unit_price * quantity
+  end
+
+  def unit_price
+    # if there is a record
+    if persisted?
+      self[:unit_price]
+    else
+      product.price
+    end
+  end
+
+  private 
+
+  def set_total 
+    self[:total] = total * quantity
+  end
+
+  def set_unit_price
+    self[:unit_price] = unit_price
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'order_items/create'
+  get 'order_items/update'
+  get 'order_items/destroy'
+  get 'cart/show'
   resources :categories
   devise_for :users
   resources :products
